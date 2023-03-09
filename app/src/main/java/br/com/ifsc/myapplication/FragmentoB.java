8ea6038c756a7a1a7ba8c6ecacdb58023851c7c1
natalia-coelho <nataliacoelho199@gmail.com>
2023-03-09 20:29:29 -0300

    activity class demo

---
+++ app/src/main/java/br/com/ifsc/myapplication/FragmentoB.java
@@ -0,0 +1,14 @@
+package br.com.ifsc.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FragmentoB extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragmento_b);
+    }
+}
