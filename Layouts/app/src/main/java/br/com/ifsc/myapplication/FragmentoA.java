d08194fb9be428837909926f0e70bda02e2fddc4
natalia-coelho <nataliacoelho199@gmail.com>
2023-03-16 23:30:02 -0300

    refactoring repo

---
+++ Layouts/app/src/main/java/br/com/ifsc/myapplication/FragmentoA.java
@@ -0,0 +1,9 @@
+package br.com.ifsc.myapplication;
+
+import androidx.fragment.app.Fragment;
+
+public class FragmentoA extends Fragment {
+    public FragmentoA(){
+        super(R.layout.activity_fragmento_a);
+    }
+}
