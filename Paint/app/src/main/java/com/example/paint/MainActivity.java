46e02d5dbe0e92f4dcaefa82ed6c468be34a646d
natalia-coelho <nataliacoelho199@gmail.com>
2023-03-22 21:42:38 -0300

    Refactoring tsnames

--- Paint/app/src/main/java/com/example/paint/MainActivity.java
+++ Paint/app/src/main/java/com/example/paint/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.example.paint;
 
 import androidx.appcompat.app.AppCompatActivity;
 
--- Paint/app/src/test/java/com/example/paint/ExampleUnitTest.java
+++ Paint/app/src/test/java/com/example/paint/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.example.paint;
 
 import org.junit.Test;
 
