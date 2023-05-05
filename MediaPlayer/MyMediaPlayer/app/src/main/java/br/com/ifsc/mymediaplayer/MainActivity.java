e5dce7bd737e29422e6e1032f8fb672cf7e40bda
natalia-coelho <nataliacoelho199@gmail.com>
2023-05-05 00:38:43 -0300

    Create a volume control bar

--- MediaPlayer/MyMediaPlayer/app/src/main/java/br/com/ifsc/mymediaplayer/MainActivity.java
+++ MediaPlayer/MyMediaPlayer/app/src/main/java/br/com/ifsc/mymediaplayer/MainActivity.java
@@ -1,6 +1,8 @@
 package br.com.ifsc.mymediaplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
@@ -13,32 +15,63 @@ import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    AudioManager audioManager;
     MediaPlayer mediaPlayer;
     ImageButton bntPlay, bntStop, bntPause;
-    SeekBar seekBar;
+    SeekBar seekBarTime, seekBarVolume;
     TextView textViewTime;
+    private Timer timerCounter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        mediaPlayer = MediaPlayer.create(this, R.raw.seminovos);
+        audioManager  = (AudioManager) getSystemService(AUDIO_SERVICE);
         mediaPlayer=mediaPlayer.create(this, R.raw.seminovos);
+
         bntPlay = findViewById(R.id.btnPlay);
         bntStop = findViewById(R.id.btnStop);
         bntPause = findViewById(R.id.btnPause);
-
 
         bntPlay.setOnClickListener(this);
         bntStop.setOnClickListener(this);
         bntPause.setOnClickListener(this);
 
-        seekBar = findViewById(R.id.seekBarTime);
+        seekBarTime = findViewById(R.id.seekBarTime);
+        seekBarVolume = findViewById(R.id.seekBarVolume);
         textViewTime = findViewById(R.id.textViewTime);
 
         textViewTime.setText(convertDurationMillis(mediaPlayer.getDuration()));
         mediaPlayer.getCurrentPosition();
         timerCounter();
+
+        seekBarTime.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if(fromUser){
+                    seekBar.setMax(mediaPlayer.getDuration());
+                    mediaPlayer.seekTo(progress);
+                }
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        startSeekBar();
+    }
+
+    public void startSeekBar(){
+        int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int currentVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+
+        seekBarVolume.setMax(maxVolume);
+        seekBarVolume.setProgress(currentVolume);
     }
 
     @Override
@@ -74,9 +107,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         textViewTime.setText(strTimeDuration);
     }
 
-    private Timer timer;
     private void timerCounter(){
-        timer = new Timer();
+        timerCounter = new Timer();
         TimerTask task = new TimerTask() {
             @Override
             public void run() {
@@ -84,10 +116,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     @Override
                     public void run() {
                         updateTimer();
+//                        seekBarTime.setProgress();
                     }
                 });
             }
         };
-        timer.schedule(task, 0, 1000);
+        timerCounter.schedule(task, 0, 1000);
     }
 }
